f013623a0d6c4307b4cd36e092c0ef609c9b9805
SOKOL491 <44403484+Sokol491@users.noreply.github.com>
2023-06-18 20:47:49 +0500

    Add files via upload

--- search.js
+++ search.js
@@ -1,49 +1,16 @@
-var input,search,pr,result,result_arr, locale_HTML, result_store;
-
-function func() {
- 	locale_HTML = document.body.innerHTML;   // сохраняем в переменную весь body (Первоначальный)
-}
-setTimeout(func, 1000);  //ждем подгрузки Jsona и выполняем
-
-function FindOnPage(name, status) {
-
-	input = document.getElementById(name).value; //получаем значение из поля в html
-	
-	if(input.length<3&&status==true)
-	{
-		alert('Для поиска вы должны ввести три или более символов');
-		function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
-	}
-	
-	if(input.length>=3)
-	{
-		function FindOnPageGo() {
-
-			search = '/'+input+'/g';  //делаем из строки регуярное выражение
-			pr = document.body.innerHTML;   // сохраняем в переменную весь body
-			result = pr.match(/>(.*?)</g);  //отсекаем все теги и получаем только текст
-			result_arr = [];   //в этом массиве будем хранить результат работы (подсветку)
-
-			var warning = true;
-			for(var i=0;i<result.length;i++) {
-				if(result[i].match(eval(search))!=null) {
-					warning = false;
-				}
-			}
-			if(warning == true) {
-				alert('Не найдено ни одного совпадения');
-			}
-
-			for(var i=0; i<result.length;i++) {
-				result_arr[i] = result[i].replace(eval(search), '<span style="background-color:yellow;">'+input+'</span>'); //находим нужные элементы, задаем стиль и сохраняем в новый массив
-			}
-			for(var i=0; i<result.length;i++) {
-				pr=pr.replace(result[i],result_arr[i])  //заменяем в переменной с html текст на новый из новогом ассива
-			}
-			document.body.innerHTML = pr;  //заменяем html код
-		}
-	}
-	function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
-	if(status) { FindOnPageBack(); FindOnPageGo(); } //чистим прошлое и Выделяем найденное
-	if(!status) { FindOnPageBack(); } //Снимаем выделение
+function search() {
+    let input = document.getElementById("inputSearch");
+    let filter = input.value.toUpperCase();
+    let ul = document.getElementById("list");
+    let li = ul.getElementsByTagName("li");
+ 
+    // Перебирайте все элементы списка и скрывайте те, которые не соответствуют поисковому запросу
+    for (let i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
