184162b28819f6475c91fa75a3420489b4208828
SOKOL491 <44403484+Sokol491@users.noreply.github.com>
2023-06-18 20:41:20 +0500

    Add files via upload

--- googleTable.js
+++ googleTable.js
@@ -1,7 +1,5 @@
 function getTable(id,gid){
 var csv, array;
-var gid = 0;
-var id = '1lSdGFGr-X-uNSNv4_3PFKJgvRNt_YOEYffCHP-WWAkM';
 csv = file_get_contents(id, gid);
 csv = explode("\r\n", $csv);
 array = array_map('str_getcsv', csv);
