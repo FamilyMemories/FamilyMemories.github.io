c8f0b4177bac18cd687da150a13a1fdd10f621ff
SOKOL491 <44403484+Sokol491@users.noreply.github.com>
2023-06-19 12:53:02 +0500

    Delete googleTable.js

--- googleTable.js
+++
@@ -1,7 +0,0 @@
-function getTable(id,gid){
-var csv, array;
-csv = file_get_contents(id, gid);
-csv = explode("\r\n", $csv);
-array = array_map('str_getcsv', csv);
-return array; // возвращаем результат
-}
